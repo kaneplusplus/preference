41038a9c8e6490903a9dfea3b3b0be7394bc5d80
Michael Kane <kaneplusplus@gmail.com>
2016-10-05 19:28:13 -0400

    Added tooltips. It would be nice if they could be turned off from the ui.

--- inst/web-app/server.r
+++ inst/web-app/server.r
@@ -1,15 +1,47 @@
 library(shiny)
 source('sample-size.r')
 
+parse_sequence_text = function(x) {
+  ret = as.numeric(x)
+  if (is.na(ret)) {
+    # Try to parse it as a sequence.
+
+    to_by = regexpr("\\d+\\.?\\d*\\s?to\\s?\\d+\\.?\\d*\\s+by", x)
+    to = regexpr("\\d+\\.?\\d*\\s?to\\s?\\d+\\.?\\d*\\s+", x)
+    if (to_by != -1) {
+      # It is a "to-by" statement?
+      vals = as.numeric(unlist(strsplit(x, "to|by")))
+      ret = seq(from=vals[1], to=vals[2], by=vals[3])
+    } else if (to != -1) {
+      # It is a "to" statement?    
+      vals = as.numeric(unlist(strsplit(x, "to")))
+      ret = seq(from=vals[1], to=vals[2], by=1)
+    } else {
+      # We can't parse it.
+      ret = NA
+    }
+  }
+  ret
+}
+
+
 # Define server logic required to draw a histogram
-server <- shinyServer(function(input, output) {
-  output$sample_size = renderTable({
+server <- shinyServer(function(input, output, session) {
+
+  get_strat_selection = reactive({
+    delta_pi = parse_sequence_text(input$delta_pi)
+    delta_nu = parse_sequence_text(input$delta_nu)
     # Turn the text inputs into vectors of numeric values.
     phi = as.numeric(gsub(" ", "", unlist(strsplit(input$phi, ","))))
     sigma2 = as.numeric(gsub(" ", "", unlist(strsplit(input$sigma2, ","))))
     xi = as.numeric(gsub(" ", "", unlist(strsplit(input$xi, ","))))
-    strat_selection(input$zbeta, phi, sigma2, input$delta_pi, input$delta_nu,
+    strat_selection(input$zbeta, phi, sigma2, delta_pi, delta_nu,
                     input$zalpha, input$theta, xi)
   })
+
+  output$sample_size = renderTable({
+    get_strat_selection()
+  })
+
 })
 
--- inst/web-app/ui.r
+++ inst/web-app/ui.r
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyBS)
 
 # Define UI for application that draws a histogram
 ui <- shinyUI(pageWithSidebar(
@@ -7,6 +8,8 @@ ui <- shinyUI(pageWithSidebar(
   headerPanel("Stratified Sample-Size Calculator"),
 
   # Need to add tooltips for descriptions of inputs.
+
+  # Pass syntax: 1 to by 0.4
    
   # Sidebar with a slider input for number of bins 
   sidebarPanel(
@@ -14,16 +17,31 @@ ui <- shinyUI(pageWithSidebar(
     sliderInput("zbeta", "Z-Power:", min=0, max=3, value=1.282),
     textInput("phi", "Preference Rate:", value="0.5, 0.5"),
     textInput("sigma2", "Within-Stratum Variances:", value="1, 1"),
-    numericInput("delta_pi", "Preference Effect:", value=1, min=0, step=0.01),
-    numericInput("delta_nu", "Selection Effect:", value=1, min=0, step=0.01),
+    textInput("delta_pi", "Preference Effect:", value="1"),
+    textInput("delta_nu", "Selection Effect:", value="1"),
     sliderInput("zalpha", "Z Type-1 Error:", min=0, max=3, value=1.96),
     sliderInput("theta", "Theta:", min=0, max=1, value=0.5),
-    textInput("xi", "Proporion of Patients in Each Stratum:", value="0.5, 0.5")
+    textInput("xi", "Proporion of Patients in Each Stratum:", value="0.5, 0.5"),
+    bsTooltip("num_strata", "PUT TEXT HERE.", "right", 
+              options(container="list")),
+    bsTooltip("zbeta", "PUT TEXT HERE.", "right", options(container="list")),
+    bsTooltip("phi", "PUT TEXT HERE.", "right", options(container="list")),
+    bsTooltip("sigma2", "PUT TEXT HERE.", "right", options(container="list")),
+    bsTooltip("delta_pi", "PUT TEXT HERE.", "right", options(container="list")),
+    bsTooltip("delta_nu", "PUT TEXT HERE.", "right", options(container="list")),
+    bsTooltip("zalpha", "PUT TEXT HERE.", "right", options(container="list")),
+    bsTooltip("theta", "PUT TEXT HERE.", "right", options(container="list")),
+    bsTooltip("xi", "PUT TEXT HERE.", "right", options(container="list"))
+    # It would be nice to get rid of tooltips when the user doesn't want
+    # them.
+    #checkboxInput("tooltip_checkbox", label="Include help tooltips", 
+    #              value=FALSE),
   ),
       
   # Show a plot of the generated distribution
   mainPanel(
-    tableOutput("sample_size")
+    tableOutput("sample_size"),
+    uiOutput("update_tooltip")
   )
 
 ))
