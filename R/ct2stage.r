2756943220102f2da75f6eb0a68857731896dc1d
Michael Kane <kaneplusplus@gmail.com>
2017-02-15 15:53:57 -0500

    Added a data documentation skeleton.

---
+++ R/ct2stage.r
@@ -0,0 +1,10 @@
+#' A data set available with ct2stage
+#'
+#' @name imap
+#' @docType data
+#' @description Here's something about the trial that this data set was taken 
+#' from.
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{http://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#' @keywords data
+NULL
