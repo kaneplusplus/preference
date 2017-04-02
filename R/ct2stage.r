ff30b7fa3ba27d8fff5fd74c7acca95f227f3fbd
bricameron <bricameron@Brianas-MacBook-Pro-2.local>
2017-04-02 10:05:15 -0400

    Updated documentation for data sets

--- R/ct2stage.r
+++ R/ct2stage.r
@@ -1,22 +1,35 @@
-#' A data set available with ct2stage
+#' Summary data for the IMAP study
 #'
 #' @name imap
 #' @docType data
-#' @description Here's something about the trial that this data set was taken 
-#' from.
-#' @references Moore et al. (2013) Genetics 195:1077-1086
-#' (\href{http://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#' @description Data set is from the Improving Management of Abnormal Pap Smears study,
+#' which used a doubly randomized preference trial design to evaluate psychosocial 
+#' outcomes in women found to have atypical cells in a Pap Smear. Two systems for
+#' managing the atypical cells were tested (repeated Pap smears or HCV triage) and a 
+#' doubly randomized design was used to evaluate the role of patient preference. The
+#' data set provides mean, standard deviation and sample sizes of the SF36 outcome for
+#' each treatment in both the choice and random arms.
+#' @references McCaffery et al. (2010) BMJ 340:b4491.
+#' (\href{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2827716/}{PubMed})
 #' @keywords data
 NULL
 
 
-#' A data set available with ct2stage
+#' Stratified summary data for the IMAP study
 #'
 #' @name imap_strat
 #' @docType data
-#' @description Here's something about the trial that this data set was taken 
-#' from.
-#' @references Moore et al. (2013) Genetics 195:1077-1086
-#' (\href{http://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#' @description Data set is from the Improving Management of Abnormal Pap Smears study,
+#' which used a doubly randomized preference trial design to evaluate psychosocial 
+#' outcomes in women found to have atypical cells in a Pap Smear. Two systems for
+#' managing the atypical cells were tested (repeated Pap smears or HCV triage) and a 
+#' doubly randomized design was used to evaluate the role of patient preference. In
+#' this data set, patients are stratified by STAI score. Stratum 1 includes patients 
+#' with low STAI scores (<=10 on STAI averaged across whole study); stratum 2 includes
+#' patients wiht high STAI scores (>10). The data set provides mean, standard 
+#' deviation and sample sizes of the SF36 outcome for each treatment in both the 
+#' choice and random arms for both strata.
+#' @references McCaffery et al. (2010) BMJ 340:b4491.
+#' (\href{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2827716/}{PubMed})
 #' @keywords data
 NULL
