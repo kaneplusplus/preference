f3716f3ea91a75dded97b19bf6dba506179ec2ae
Michael Kane <kaneplusplus@gmail.com>
2017-05-03 12:27:48 -0400

    Initial commit. Utitlities for the web app.

---
+++ R/util.r
@@ -0,0 +1,40 @@
+
+create_strat_selection_plot = function(params, df) {
+  ret = NULL
+  if (length(unique(df$delta_pi)) > 1 && length(unique(df$delta_nu)) == 1) {
+    ret = ggplot(data=df, aes(x=delta_pi, y=sample_size)) +
+      geom_line() + xlab("Preference Effect") + ylab("Sample Size")
+  }
+  else if (length(unique(df$delta_pi)) == 1 &&
+           length(unique(df$delta_nu)) > 1) {
+    ret = ggplot(data=df, aes(x=delta_nu, y=sample_size)) +
+      geom_line() + xlab("Selection Effect") + ylab("Sample Size")
+  }
+  else if (length(unique(df$delta_pi)) > 1 &&
+           length(unique(df$delta_nu)) > 1) {
+    uss = length(unique(df$sample_size))
+    ret=ggplot(data=df, aes(x=delta_pi, y=delta_nu, fill=factor(sample_size)))+
+      geom_tile() + xlab("Preference Effect") + ylab("Selection Effect") +
+      scale_fill_manual(values=rev(heat.colors(uss)),
+        guide=guide_legend(title="Sample Size"))
+      #scale_fill_continuous(guide=guide_legend(title = "Sample Size")) 
+  }
+  ret
+}
+
+create_strat_selection_df = function(params) {
+  df = data.frame(
+    list(delta_pi=rep(params$delta_pi, each=length(params$delta_nu)),
+         delta_nu=rep(params$delta_nu, times=length(params$delta_pi))))
+  ss = rep(NA, nrow(df))
+  for (i in 1:nrow(df)) {
+    ss[i] = 
+      ceiling(n_sel(params$power, params$phi, params$sigma2, 
+                              df$delta_pi[i], df$delta_nu[i], params$alpha,
+                              params$theta, params$xi, params$num_strata))
+  }
+  df$sample_size = ss
+  names(df) = c("preference_effect", "selection_effect", "sample_size")
+  df     
+}
+
